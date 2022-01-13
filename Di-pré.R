37097dcca72cd5f62a9823d91eb53d27c8a2726f
Marcelo Vilas Boas <91570868+mvboas@users.noreply.github.com>
2022-01-13 15:45:36 -0300

    Update Di-pré.R

--- Di-pré.R
+++ Di-pré.R
@@ -1,12 +1,12 @@
-#Script para pegar dados da tabela DI-pré da b3
-#Adaptado por: Marcelo Vilas Boas de Castro
-#última atualização: 05/11/2020
+#Script para pegar dados da tabela DI-prÃ© da b3
+#Feito por: Marcelo Vilas Boas de Castro
+#Ãºltima atualizaÃ§Ã£o: 05/11/2020
 
-#Definindo diretórios a serem utilizados
+#Definindo diretÃ³rios a serem utilizados
 getwd()
 setwd("C:\\Users\\User\\Documents")
 
-#Carregando pacotes que serão utilizados
+#Carregando pacotes que serÃ£o utilizados
 library(stringr)
 library(rvest)
 library(stats)
@@ -15,13 +15,13 @@ bovespa <- 'http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-taxas-refer
 html <- read_html(bovespa) 
 
 dados_bov <- html %>%
-  html_nodes(xpath = '//*[@id="tb_principal1"]')%>% #aqui você já tem a tabela, daqui para baixo o código apenas padroniza a tabela que vem desestruturada
+  html_nodes(xpath = '//*[@id="tb_principal1"]')%>% #aqui vocÃª jÃ¡ tem a tabela, daqui para baixo o cÃ³digo apenas padroniza a tabela que vem desestruturada
   html_text()%>%
   str_replace(",", ".") %>%
   str_replace_all('(\r|\n){1,}',';')
 
 
-dados_bov=strsplit(dados_bov,';')[[1]][5:892]%>% #transforma a string em uma lista e elimina o cabeçalho da base
+dados_bov=strsplit(dados_bov,';')[[1]][5:892]%>% #transforma a string em uma lista e elimina o cabeÃ§alho da base
   matrix(ncol = 3, byrow = TRUE)%>%
   as.data.frame()%>%
   setNames(c("Dias Coriidos", "DI_pre_252", "DI_pre_360"))
